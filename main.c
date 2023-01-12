4e53b93f9b1a14c1f5382f11ef100c304d4b3288
Sebastian 'Swift Geek' Grzywna <swiftgeek@gmail.com>
2023-01-12 11:57:30 +0100

    Reduce nesting in stm32f1_sramboot_fix()

--- main.c
+++ main.c
@@ -76,38 +76,38 @@ STDIO_ALIAS(stdout);
 
 __attribute__((constructor)) static void stm32f1_sramboot_fix(void) {
   // Confirm whether this function is located in SRAM
-  if ( ( (int)stm32f1_sramboot_fix & SRAM_BASE ) == SRAM_BASE ) {
-    // Disable faults and set BFHFNMIGN to continue on a BusFault
-    cm_disable_faults();
-    SCB_CCR = SCB_CCR | SCB_CCR_BFHFNMIGN;
-    // Read but discard value, we are only interested in BusFault
-    // In case of SRAM boot 0x10 is the first address beyond bogus MaskROM,
-    // and nothing is mapped at that address.
-    // If flash boot was used, then range 0x0000 0000 - 0x0800 0000 would be
-    // aliased to flash instead.
-    MMIO32(0x10);
-    SCB_CCR = SCB_CCR ^ SCB_CCR_BFHFNMIGN;
-    cm_enable_faults();
-    if (SCB_CFSR == (SCB_CFSR_PRECISERR | SCB_CFSR_BFARVALID) ) {
-      // BOOTx is configured for SRAM boot, as reading 0x10 caused BusFault
-      // Clear BusFault we caused from CFSR
-      SCB_CFSR = SCB_CFSR_PRECISERR | SCB_CFSR_BFARVALID;
-      if (SCB_VTOR == 0 ) {
-        // First run after reset
-        // Mind that we only have bogus 16 byte vector table from MaskROM,
-        // so modifying any code happening including and before this
-        // is not recommended.
-        // In case of any issues, retry without SRAM boot workaround on flash,
-        // with BOOTx pins set to flash boot.
-        SCB_VTOR = SRAM_BASE;
-        /* Initialize master stack pointer. */
-         __asm__ volatile("msr msp, %0"::"g"
-            (*(volatile uint32_t *)SRAM_BASE));
-        /* Load address of reset_handler() from vector_table and run it */
-        (*(void (**)())(SRAM_BASE + 4))();
-      }
-    }
-  }
+  if ( ( (int)stm32f1_sramboot_fix & SRAM_BASE ) != SRAM_BASE )
+    return;
+  // Disable faults and set BFHFNMIGN to continue on a BusFault
+  cm_disable_faults();
+  SCB_CCR = SCB_CCR | SCB_CCR_BFHFNMIGN;
+  // Read but discard value, we are only interested in BusFault
+  // In case of SRAM boot 0x10 is the first address beyond bogus MaskROM,
+  // and nothing is mapped at that address.
+  // If flash boot was used, then range 0x0000 0000 - 0x0800 0000 would be
+  // aliased to flash instead.
+  MMIO32(0x10);
+  SCB_CCR = SCB_CCR ^ SCB_CCR_BFHFNMIGN;
+  cm_enable_faults();
+  if (SCB_CFSR != (SCB_CFSR_PRECISERR | SCB_CFSR_BFARVALID) )
+    return;
+  // BOOTx is configured for SRAM boot, as reading 0x10 caused BusFault
+  // Clear BusFault we caused from CFSR
+  SCB_CFSR = SCB_CFSR_PRECISERR | SCB_CFSR_BFARVALID;
+  if (SCB_VTOR != 0 )
+    return;
+  // First run after reset
+  // Mind that we only have bogus 16 byte vector table from MaskROM,
+  // so modifying any code happening including and before this
+  // is not recommended.
+  // In case of any issues, retry without SRAM boot workaround on flash,
+  // with BOOTx pins set to flash boot.
+  SCB_VTOR = SRAM_BASE;
+  /* Initialize master stack pointer. */
+   __asm__ volatile("msr msp, %0"::"g"
+      (*(volatile uint32_t *)SRAM_BASE));
+  /* Load address of reset_handler() from vector_table and run it */
+  (*(void (**)())(SRAM_BASE + 4))();
 }
 
 
